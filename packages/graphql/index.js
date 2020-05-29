22725444b1528ff72769476d802b690a86904981
AmirRezaDalir <dalirnet@gmail.com>
2020-05-29 22:12:29 +0430

    graphql start

---
+++ packages/graphql/index.js
@@ -0,0 +1,44 @@
+// lib
+import { GraphQLClient } from "graphql-request";
+
+export default {
+  data() {
+    return {
+      graphqlEndpoint: "http://localhost/graphql",
+      graphqlHeaders: {},
+      graphqlLoading: false,
+      graphqlData: null,
+      graphqlError: null,
+      graphqlClient: new GraphQLClient(this.graphqlEndpoint, {
+        headers: this.graphqlHeaders,
+      }),
+    };
+  },
+
+  methods: {
+    graphqlRequest(query, variables = {}, loadingListenner = null) {
+      return new Promise((resolve, reject) => {
+        this.graphqlLoading = true;
+        if (typeof loadingListenner == "function") {
+          loadingListenner(this.graphqlLoading);
+        }
+        this.graphqlClient
+          .request(query, variables)
+          .then((data) => {
+            this.graphqlData = data;
+            resolve(this.graphqlData);
+          })
+          .catch((error) => {
+            this.graphqlError = error.toString();
+            reject(this.graphqlError);
+          })
+          .finally(() => {
+            this.graphqlLoading = false;
+            if (typeof loadingListenner == "function") {
+              loadingListenner(this.graphqlLoading);
+            }
+          });
+      });
+    },
+  },
+};
